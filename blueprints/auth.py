18cf8e393ca0d287e02872c74eb2f3895eb99477
Cuprous-Hub <Omosebiking4@gmail.com>
2026-08-13 14:58:45 +0100

    Fix formatting and comments in auth.py

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,6 +1,7 @@
 #Dunwo remeber the leadcity?!
 from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
 from flask_login import login_user, logout_user, login_required, current_user
+#air???
 
 from extensions import db
 from models import User
